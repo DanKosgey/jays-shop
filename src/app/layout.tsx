8b5ec5e87de9771298dba6bcbb6e1212b7fc5f00
Your Name <kosgeidan3@gmail.com>
2025-10-30 23:36:11 +0300

    Fix font loading error by switching from self-hosted to Google Fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,20 +15,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Local font support */}
-        <link rel="preload" href="/fonts/inter-var-latin.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
-        <style dangerouslySetInnerHTML={{
-          __html: `
-            @font-face {
-              font-family: 'Inter';
-              font-style: normal;
-              font-weight: 100 900;
-              font-display: swap;
-              src: url('/fonts/inter-var-latin.woff2') format('woff2');
-              unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-            }
-          `
-        }} />
+        {/* Google Fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet" />
       </head>
       <body>
         <ClientLayout>
